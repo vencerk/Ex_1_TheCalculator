734324d843c2cae64d26177ee3eede968713b0b3
vencerk <33147126+vencerk@users.noreply.github.com>
2018-11-29 18:46:07 +0800

    取消按钮转换，改为横竖屏转换

--- app/src/main/java/com/example/ex_1_thecalculator/MainActivity.java
+++ app/src/main/java/com/example/ex_1_thecalculator/MainActivity.java
@@ -1,25 +1,45 @@
 package com.example.ex_1_thecalculator;
 
-import android.content.Intent;
+
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button but=findViewById(R.id.button1Reversal);
-        but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(intent);
-            }
-        });
+        if(this.getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            //final TextView tv=(TextView) findViewById(R.id.t1);
+
+
+            Button butDel = findViewById(R.id.buttonDelete);
+            butDel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, R.string.success, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+        else if(this.getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            Button but2 = findViewById(R.id.button2);
+            but2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "222222", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
